990a6336e246ce7e2f19f96e4a093185af4e69c6
eerkaijun <kaijuneer@gmail.com>
2023-06-11 18:23:14 +0100

    transaction verifier rename

--- contracts/contracts/verifiers/Verifier.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
-
-import {TransactionVerifier} from "@zrclib/circuits/generated/TransactionVerifier.sol";
-
-contract Verifier is TransactionVerifier {}
