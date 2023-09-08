f2efbdcbc5b7cd6df3bf85278cf2f6a1bb909c64
eerkaijun <kaijuneer@gmail.com>
2023-09-08 14:22:22 +0800

    scaffolding for invariant

---
+++ contracts/test/MultiAssetShieldedPool.t.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "forge-std/Test.sol";
+import "../contracts/MultiAssetShieldedPool.sol";
+
+contract MultiAssetShieldedPoolTest is Test {
+
+    MultiAssetShieldedPool public shieldedPool;
+
+    function setUp() public {
+        
+    }
+
+    function testDeposit() public {
+        
+    }
+    
+}
