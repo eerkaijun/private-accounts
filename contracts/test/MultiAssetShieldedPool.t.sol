cf9d9d591d88f88c750b119c0355cc51a4d6ccc9
eerkaijun <kaijuneer@gmail.com>
2023-09-09 18:17:50 +0800

    sync

--- contracts/test/MultiAssetShieldedPool.t.sol
+++ contracts/test/MultiAssetShieldedPool.t.sol
@@ -2,17 +2,22 @@
 pragma solidity ^0.8.9;
 
 import "forge-std/Test.sol";
-import "../contracts/MultiAssetShieldedPool.sol";
+import { MultiAssetShieldedPool } from "../contracts/MultiAssetShieldedPool.sol";
+import { SwapExecutor } from "../contracts/abstracts/SwapExecutor.sol";
+import { TransactionVerifier } from "../contracts/verifiers/TransactionVerifier.sol";
 
 contract MultiAssetShieldedPoolTest is Test {
 
     MultiAssetShieldedPool public shieldedPool;
 
     function setUp() public {
-        
+        // TODO: deploy hasher contract using cheatcode
+        TransactionVerifier verifier = new TransactionVerifier();
+        SwapExecutor swapExecutor = new SwapExecutor();
+        shieldedPool = new MultiAssetShieldedPool(address(0), address(verifier), address(swapExecutor));
     }
 
-    function testDeposit() public {
+    function invariant_testDeposit() public {
         
     }
     
