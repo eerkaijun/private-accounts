81284e22de5b90ebec9d4eec5714f132895cee85
eerkaijun <kaijuneer@gmail.com>
2023-09-25 23:21:34 +0100

    wallet build

--- wallet/tasks/copy_circuit_files.sh
+++ wallet/tasks/copy_circuit_files.sh
@@ -6,5 +6,5 @@ cp -r ../circuits/compiled/transaction* ./public/circuits
 cp ../contracts/artifacts/contracts/mocks/MockErc20.sol/MockErc20.json ./contracts/
 cp ../contracts/artifacts/contracts/MultiAssetShieldedPool.sol/MultiAssetShieldedPool.json ./contracts/
 cp ../contracts/artifacts/contracts/mocks/MockSwapRouter.sol/MockSwapRouter.json ./contracts/
-cp ../contracts/artifacts/contracts/SwapExecutor.sol/SwapExecutor.json ./contracts/
+cp ../contracts/artifacts/contracts/abstracts/SwapExecutor.sol/SwapExecutor.json ./contracts/
 cp -r ../contracts/typechain-types ./typechain-types
